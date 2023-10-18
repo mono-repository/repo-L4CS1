b372f0c4fb81d7f57cd03fbc33830e559a7e69a6
mono-repository <1911140076s@ed.fuk.kindai.ac.jp>
2023-10-18 07:06:06 +0000

    L4-CS1

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationController < ActionController::Base
+    #授業資料第4回の資料参考
+    #https://classroom.google.com/u/1/c/NjIxMzg1MDg3MzU5/m/NTMwMDg1MjEwNjQx/details
+    # def L4
+    #     cookies[:c] ||= 0 
+    #     cookies[:c] = cookies[:c].to_i + 1
+    #     render plain: cookies[:c]
+    # end
+    
+    # def L4
+    #     session[:c] ||= 0
+    #     session[:c] = session[:c].to_i + 1
+    #     render plain: session[:c]
+    # end
+    
+end
